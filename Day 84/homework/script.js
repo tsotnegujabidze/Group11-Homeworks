091e98c0e6e556a24627c46521b9d9f3da038999
Paata634 <tsotnegujabidze2@gmail.com>
2024-11-01 13:08:47 +0400

    commit

--- Day 84/homework/script.js
+++ Day 84/homework/script.js
@@ -1,14 +1,15 @@
-function Car(make, model, year, color) {
-    this.make = make;
-    this.model = model;
-    this.year = year;
-    this.color = color;
-    
-    this.getCarInfo = function() {
-        return `${this.year} ${this.make} ${this.model} - ${this.color}`;
-    };
+function Submit(firstname, lastname, email) {
+    this.firstname = firstname;
+    this.lastname = lastname;
+    this.email = email;
+}
+function submit() {
+    const firstname = document.getElementById("firstname").value;
+    const lastname = document.getElementById("lastname").value;
+    const email = document.getElementById("email").value;
+
+    const person1 = new Submit(firstname, lastname, email);
+
+    alert(person1.firstname + ' ' + person1.lastname + ' ' + person1.email)
 }
 
-const myCar = new Car('Toyota', 'Corolla', 2021, 'White');
-
-console.log(myCar.getCarInfo());
